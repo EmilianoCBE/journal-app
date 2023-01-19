6d4ad1dad08cd628dd3378ec27eb1bd342dccc1e
EmilianoCBE <e.carrizales1305@gmail.com>
2023-01-18 22:10:12 -0600

    Sidebar added

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,45 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Drawer, Box, Toolbar, Typography, Divider, List, ListItem, ListItemButton, ListItemIcon, Grid, ListItemText } from "@mui/material"
+
+export const SideBar = ({drawerWidth = 240}) => {
+  return (
+    <Box
+        component='nav'
+        sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+    >
+        <Drawer
+            variant="permanent" //Temporary
+            open
+            sx={{
+                display: {xs: 'block'},
+                '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth}
+            }}
+        >
+            <Toolbar>
+                <Typography variant="h6" noWrap component='div'>
+                    Elden Ring
+                </Typography>
+            </Toolbar>
+            <Divider />
+
+            <List>
+                {
+                    ['January', 'February', 'March', 'April', 'May'].map(text =>(
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={text}/>
+                                    <ListItemText secondary={'lorem ipsum'}/>
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+        </Drawer>
+    </Box>
+  )
+}
