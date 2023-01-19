fddfaa21d3c71207381f67d56794f8e15a8687e7
EmilianoCBE <e.carrizales1305@gmail.com>
2023-01-18 22:30:12 -0600

    NothingSelected View added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,13 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam assumenda neque, sed autem possimus corporis soluta odit facilis facere hic perspiciatis impedit rerum dignissimos, amet voluptatum molestias dolores aspernatur nihil!
       
-        {/* Nothing selected */}
+        <NothingSelectedView />
 
         {/* Noteview */}
-      </Typography>
     </JournalLayout>
   )
 }
